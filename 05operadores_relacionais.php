f372818a4cdbcb2f02877733c91f40bbfdb2e158
unknown <abreuroberto2015@gmail.com>
2021-07-09 19:20:47 -0300

    fim

---
+++ 05operadores_relacionais.php
@@ -0,0 +1,10 @@
+<?php
+    $numero1 = 9;
+    $numero2 = 2;
+
+    if($numero1 > $numero2){
+        echo $numero1. " é maior que ".$numero2;
+    }else{
+        echo $numero2." é maior que ".$numero1
+    }
+?>
