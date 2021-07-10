8627ddf0043e180a21025187ba3946d4c71e3625
unknown <abreuroberto2015@gmail.com>
2021-07-10 18:09:48 -0300

    fim5

--- 05operadores_relacionais.php
+++ 05operadores_relacionais.php
@@ -5,6 +5,6 @@
     if($numero1 > $numero2){
         echo $numero1. " é maior que ".$numero2;
     }else{
-        echo $numero2." é maior que ".$numero1
+        echo $numero2." é maior que ".$numero1;
     }
 ?>
